6d1d85df0f4e59f9dd846571ce639588e6a18a3c
Vivek <vivektheintel@gmail.com>
2020-12-11 07:59:00 +0530

    Getting rid of not required basic-error-controller

--- src/main/java/com/vv/personal/twm/tribalwars/TribalWarsServer.java
+++ src/main/java/com/vv/personal/twm/tribalwars/TribalWarsServer.java
@@ -6,11 +6,14 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * @author Vivek
  * @since 29/11/20
- *
+ * <p>
  * Access point changed to <__>:23555/swagger-ui/index.html
  */
 @EnableFeignClients
@@ -25,5 +28,13 @@ public class TribalWarsServer {
     @Bean
     ProtobufHttpMessageConverter protobufHttpMessageConverter() {
         return new ProtobufHttpMessageConverter();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.vv.personal.twm.tribalwars"))
+                .build();
     }
 }
