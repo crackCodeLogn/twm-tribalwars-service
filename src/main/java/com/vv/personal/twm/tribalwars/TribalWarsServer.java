787c8592180f26c87b9bd3a1904995f09d16fa23
Vivek <vivektheintel@gmail.com>
2021-01-23 20:51:59 +0530

    Mundane augment

--- src/main/java/com/vv/personal/twm/tribalwars/TribalWarsServer.java
+++ src/main/java/com/vv/personal/twm/tribalwars/TribalWarsServer.java
@@ -1,14 +1,25 @@
 package com.vv.personal.twm.tribalwars;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static com.vv.personal.twm.tribalwars.constants.Constants.*;
 
 /**
  * @author Vivek
@@ -20,6 +31,10 @@ import springfox.documentation.spring.web.plugins.Docket;
 @EnableEurekaClient
 @SpringBootApplication
 public class TribalWarsServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TribalWarsServer.class);
+
+    @Autowired
+    private Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(TribalWarsServer.class, args);
@@ -36,5 +51,17 @@ public class TribalWarsServer {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.vv.personal.twm.tribalwars"))
                 .build();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void firedUpAllCylinders() {
+        String host = LOCALHOST;
+        try {
+            host = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            LOGGER.error("Failed to obtain ip address. ", e);
+        }
+        String port = environment.getProperty(LOCAL_SPRING_PORT);
+        LOGGER.info("TW Server activation is complete! Exact url: {}", String.format(SWAGGER_UI_URL, host, port));
     }
 }
--- src/main/java/com/vv/personal/twm/tribalwars/constants/Constants.java
+++ src/main/java/com/vv/personal/twm/tribalwars/constants/Constants.java
@@ -9,4 +9,15 @@ public class Constants {
     public enum SCREEN_TYPE {
         FARM, REPORT
     }
+
+    //FORMATTERS
+    public static final String HEROKU_SWAGGER_UI_URL = "https://%s/swagger-ui/index.html";
+    public static final String SWAGGER_UI_URL = "http://%s:%s/swagger-ui/index.html";
+    public static final String HEROKU_HOST_URL = "https://%s";
+    public static final String HOST_URL = "http://%s:%s";
+
+    public static final String LOCALHOST = "localhost";
+    public static final String LOCAL_SPRING_HOST = "local.server.host";
+    public static final String LOCAL_SPRING_PORT = "local.server.port";
+    public static final String SPRING_APPLICATION_HEROKU = "spring.application.heroku";
 }
